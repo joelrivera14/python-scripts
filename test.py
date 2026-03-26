9b9f1e7bb41d6d7a7f70d0685b8be136e4fe48f0
joel rivera <jorivera114@gmail.com>
2026-03-26 19:02:56 -0400

    second test\self.assertIn(unittest.mock.call(),mock_print.mock_calls\with multiple checks

--- test.py
+++ test.py
@@ -9,5 +9,23 @@ class TestingFunctions(unittest.TestCase):
         main.main()
         mock_print.assert_called_with('the average is 2.00')
 
+    @patch('builtins.input', side_effect=['e','-1','2','0','4'])
+    @patch('builtins.print')
+    def testing_except_and_else(self, mock_print, mock_input):
+        main.main()
+        self.assertIn(
+            unittest.mock.call('Please enter a number'),
+            mock_print.mock_calls
+        )
+        self.assertIn(
+            unittest.mock.call('Enter a number greater than 0'),
+            mock_print.mock_calls
+        )
+        self.assertIn(
+            unittest.mock.call('the average is 2.00'),
+            mock_print.mock_calls
+        )
+    
+
 if __name__ == "__main__":
     unittest.main()
