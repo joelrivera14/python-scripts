12cd1c91371e83593bb3ebec11e1d0024e2342d0
joel rivera <jorivera114@gmail.com>
2026-03-27 14:55:16 -0400

    validating_users

--- validating_user/validating_test.py
+++ validating_user/validating_test.py
@@ -4,7 +4,19 @@ from validate import validate_user
 class TestValidate(unittest.TestCase):
     def test_basic(self):
         name = 'joel'
-        minLen = 10
+        minLen = 3
+        expected = True
+        self.assertEqual(validate_user(name, minLen), expected)
+    
+    def test_special_chars(self):
+        name = "**(()(&^%$@@!))"
+        minLen = 5
+        expected = False
+        self.assertEqual(validate_user(name, minLen), expected)
+    
+    def test_not_enough_chars(self):
+        name = 'Joel'
+        minLen = 5
         expected = False
         self.assertEqual(validate_user(name, minLen), expected)
 
